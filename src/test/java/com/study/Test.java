a50bd602e204e74b1749f0e5184dd89160359ba6
zblfendou <zhangbinleifendou@163.com>
2017-06-20 17:49:32 +0800

    dubbo配置修改为引用配置文件数据

---
+++ src/test/java/com/study/Test.java
@@ -0,0 +1,14 @@
+package com.study;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by zbl on 2017/6/20.
+ */
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+
+    }
+}
