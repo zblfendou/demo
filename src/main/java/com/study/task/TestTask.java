c83b2ff0bcec9b09678edffd33f618a8c7ca548c
zblfendou <zhangbinleifendou@163.com>
2017-06-21 17:29:02 +0800

    优化

--- src/main/java/com/study/task/TestTask.java
+++ src/main/java/com/study/task/TestTask.java
@@ -1,12 +1,14 @@
 package com.study.task;
 
+import com.study.models.user.User;
+import com.study.services.user.UserService;
 import com.study.task.TimedTask;
 import org.springframework.context.ApplicationContext;
 
 /**
  * Created by zbl on 2017/6/20.
  */
-public class TestTask extends TimedTask{
+public class TestTask extends TimedTask {
     private static final long serialVersionUID = 2534578336318827874L;
 
     @Override
@@ -21,6 +23,11 @@ public class TestTask extends TimedTask{
 
     @Override
     public void executeTask(ApplicationContext context) {
+        UserService service = getService(context, UserService.class);
+        User user = new User();
+        user.setName("test task user");
+        user.setPassword("test task password");
+        service.add(user);
         System.out.println("do testTask over!!!");
     }
 }
